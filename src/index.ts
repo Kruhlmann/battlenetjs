918205ab0426f98554c8170add725c477a2fe5e4
Andreas Krühlmann <andreas@kruhlmann.dev>
2022-06-29 16:35:10 +0200

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { BattleNetNamespace } from "./data/namespace/namespace";
+import { BattleNetOauthService } from "./domain/auth/oauth_service";
 import { BattleNetClient } from "./domain/client";
 
 module.exports = {
     BattleNetClient,
+    BattleNetOauthService,
+    BattleNetNamespace,
 }
